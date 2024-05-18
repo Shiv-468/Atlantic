9b3c80e20c532c6c613921d8525f125f920f7ad9
Shiv-468 <shivmuwal.147@gmail.com>
2024-05-18 14:17:54 +0530

    add navbar

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,35 +1,52 @@
-import React from 'react'
+import React,{ useState }  from 'react'
 import Logo from '../assets/images/svg/logo.svg'
 import SearchSvg from '../assets/images/svg/SearchSvg.svg'
+import DropDownArrow from '../assets/images/svg/DropDownArrow.svg'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 const HeroSection = () => {
+  const [data , setdata]=useState(false);
+  function view (){setdata(!data)
+ if(data === false){
+     document.body.classList.add("overflow_hidden");
+ }
+ else{
+     document.body.classList.remove("overflow_hidden");
+ }
+ }
   return (
-    <section className='Hero_Bg min_height_777 d-flex flex-column'>
+    <section className='Hero_Bg min-vh-100 d-flex flex-column overflow-hidden'>
       <nav className='nav_bg position-relative'>
         <div className=" container max_width_1360 nav_padding">
           <div className=" d-flex align-items-center justify-content-between">
             <div className=" d-flex align-items-center gap-3 gap_55 ">
               <img src={Logo} alt="logo" />
-              <ul className=' mb-0 ps-0 d-lg-flex align-items-center gap-3 gap_28 d-no ne d-lg-b lock mobile_view'>
+              <ul className={`${data ? "navShow":"navhide"} gap-4 gap_28 d-flex align-items-center mobile_view ps-0 mb-0`}>
                 <li><a href="#" className=' text-white text-nowrap font_size_sm line_height_md font_family_DM_Sans fw-normal'>About Us</a></li>
-                <li><a href="#" className=' text-white font_size_sm line_height_md font_family_DM_Sans fw-normal'>Categories</a></li>
+                <li class="nav-item dropdown">
+                  <DropdownButton id="dropdown-basic-button" title="Categories">
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                  </DropdownButton>
+                </li>
                 <li><a href="#" className=' text-white font_size_sm line_height_md font_family_DM_Sans fw-normal'>Services</a></li>
                 <li><a href="#" className=' text-white font_size_sm line_height_md font_family_DM_Sans fw-normal'>Testimonials</a></li>
                 <li><a href="#" className=' text-white font_size_sm line_height_md font_family_DM_Sans fw-normal'>Blogs</a></li>
               </ul>
             </div>
-            <input type="checkbox" id="menu_box" hidden />
-            <label htmlFor="menu_box" className='menu_box'>
-              <span class="line"></span>
-              <span class="line mt-10 mb-10"></span>
-              <span class="line"></span>
-            </label>
+            <div onClick={view} className="menuicon d-flex flex-column gap-2 d-lg-none">
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
             <div className=" d-flex align-items-center gap-3 d-none d-lg-block">
               <form action="/action_page.php" className=' d-flex align-items-center gap-3'>
                 <div className=" input_div d-flex align-items-center">
                   <span>
                     <img src={SearchSvg} alt="SearchSvg" />
                   </span>
-                  <input type="text" id='none' placeholder='Search' className=' py-3' />
+                  <input type="text" id='none' placeholder='Search' className=' py-3 font_size_xmd line_height_sm text-white' />
                 </div>
                 <button className='common_button text-white text-nowrap font_family_chivo font_size_sm line_height_md font_weight_900 text-uppercase '>Get in touch</button>
               </form>
@@ -37,6 +54,7 @@ const HeroSection = () => {
           </div>
         </div>
       </nav>
+
       <div className=" d-flex align-items-end justify-content-start flex-grow-1">
         <div className=" container max_width_1140">
           <div className=" d-flex justify-content-between flex-column flex-sm-row align-items-start align-items-md-end pb-3">
